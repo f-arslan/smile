7f2c192569cb9302b425756a9d720f3e2bdaddbf
f-arslan <buildsmstff@gmail.com>
2023-07-21 00:24:50 +0300

    OnboardingScreen.kt implemented

--- app/src/main/java/com/smile/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/com/smile/ui/screens/OnboardingScreen.kt
@@ -1,54 +1,108 @@
 package com.smile.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.smile.R.drawable as AppDrawable
+import com.smile.common.composables.DefaultButton
+import com.smile.common.composables.DefaultOutlinedButton
 import com.smile.util.Constants.HIGH_PADDING
 import com.smile.util.Constants.IMAGE_SIZE
+import com.smile.util.Constants.MAX_PADDING
+import com.smile.util.Constants.MEDIUM_PADDING
 import com.smile.util.Constants.NO_PADDING
+import com.smile.util.Constants.SMALL_PADDING
+import com.smile.util.Constants.VERY_MAX_PADDING
+import java.util.Locale
+import com.smile.R.drawable as AppDrawable
+import com.smile.R.string as AppText
 
 @Composable
 fun OnBoardingScreen(popUpAndNavigate: (String) -> Unit = {}) {
     Column(modifier = Modifier.fillMaxSize()) {
         OnboardingBanner(modifier = Modifier.weight(0.5f))
-        OnboardingButtons()
+        OnboardingButtons(
+            modifier = Modifier.weight(0.5f),
+            onLoginClick = {},
+            onRegisterClick = {}
+        )
     }
 }
+
 @Composable
 fun OnboardingBanner(modifier: Modifier) {
-    val isDarkTheme = isSystemInDarkTheme()
     Surface(
         modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(NO_PADDING, NO_PADDING, HIGH_PADDING, HIGH_PADDING)
+        shape = RoundedCornerShape(NO_PADDING, NO_PADDING, VERY_MAX_PADDING, VERY_MAX_PADDING),
+        color = MaterialTheme.colorScheme.surfaceTint,
+        shadowElevation = SMALL_PADDING
     ) {
-        val image = if (isDarkTheme) {
-            painterResource(id = AppDrawable.logo_white)
-        } else {
-            painterResource(id = AppDrawable.logo_black)
-        }
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-             Image(painter = image, contentDescription = "Logo", modifier = Modifier.size(IMAGE_SIZE))
+        val image = painterResource(id = AppDrawable.logo_white)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = image,
+                contentDescription = "Logo",
+                modifier = Modifier.size(IMAGE_SIZE)
+            )
+            Text(
+                text = stringResource(id = AppText.app_name).uppercase(Locale.ROOT),
+                style = MaterialTheme.typography.displayLarge
+            )
+            Text(
+                text = stringResource(id = AppText.onboarding_slogan),
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
 
 }
 
 @Composable
-fun OnboardingButtons() {
-
+fun OnboardingButtons(modifier: Modifier, onLoginClick: () -> Unit, onRegisterClick: () -> Unit) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(VERY_MAX_PADDING),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = AppText.onboarding_title),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Spacer(Modifier.height(MEDIUM_PADDING))
+        Text(
+            text = stringResource(id = AppText.onboarding_subtitle),
+            style = MaterialTheme.typography.titleMedium,
+            minLines = 2,
+            textAlign = TextAlign.Center,
+        )
+        Spacer(Modifier.height(MAX_PADDING))
+        Row(horizontalArrangement = Arrangement.spacedBy(HIGH_PADDING)) {
+            DefaultButton(AppText.login, onLoginClick)
+            DefaultOutlinedButton(AppText.register, onRegisterClick)
+        }
+    }
 }
 
 
